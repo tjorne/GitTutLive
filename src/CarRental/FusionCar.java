d739eff5c96d9199d7b53a060495d496fa563eae
crstrskp <jesper@tjorne.dk>
2022-03-29 15:06:19 +0200

    Implemented FusionCar.java

---
+++ src/CarRental/FusionCar.java
@@ -0,0 +1,16 @@
+package CarRental;
+
+public class FusionCar extends AFuelCar
+{
+
+    public FusionCar(String regNr, String make, String model, int doors, int kmPrLiter)
+    {
+        super(regNr, make, model, doors, kmPrLiter);
+    }
+
+    @Override
+    String getFuelType()
+    {
+        return null;
+    }
+}
